ae2f2edbbbe5e5d2aa3c56adaa2553d7f5222e59
James Stanton <jestanton@outlook.com>
2019-12-01 15:28:17 +0000

    user id and auth streamlining

--- database/database.go
+++ database/database.go
@@ -2,13 +2,16 @@ package database
 
 import (
 	"database/sql"
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/alexedwards/argon2id"
 	_ "github.com/go-sql-driver/mysql" // mysql driver
+	"github.com/rs/xid"
 )
 
 var db *sql.DB
@@ -27,7 +30,7 @@ var dburl = os.Getenv("db_url")
 var dbport = os.Getenv("db_port")
 var dbname = os.Getenv("db_name")
 var dbtable = os.Getenv("db_table")
-var psCreateAccount = fmt.Sprintf("INSERT INTO `%v`.`%v` (`name`, `saltedpasswordhash`, `created`) VALUES ( ?, ?, NOW());", dbname, dbtable)
+var psCreateAccount = fmt.Sprintf("INSERT INTO `%v`.`%v` (`user_id`, `name`, `saltedpasswordhash`) VALUES (?, ?, ?);", dbname, dbtable)
 var psCheckName = fmt.Sprintf("SELECT EXISTS(SELECT * FROM `%v`.`%v` WHERE name = ?);", dbname, dbtable)
 var psCheckAuth = fmt.Sprintf("SELECT saltedpasswordhash, banned FROM `%v`.`%v` WHERE name = ?;", dbname, dbtable)
 
@@ -65,14 +68,55 @@ func CreateUser(username string, password string) (err error) {
 	if err != nil {
 		return err
 	}
-	fmt.Println(hash)
-	_, err = statement.Exec(username, hash)
+
+	guid := xid.New()
+
+	_, err = statement.Exec(guid, username, hash)
 
 	return err
 }
 
-// CheckPassword returns true if the username/password combo matches, or false if it doesnt match (or the username does not exists)
-func CheckPassword(username string, password string) (valid bool, err error) {
+// ProcessAuth processed a request to see if the authentication is valid
+func ProcessAuth(headers map[string]string) (status int, msg string) {
+	if authHeader, ok := headers["Authorization"]; ok {
+		decodedHeader, err := base64.StdEncoding.DecodeString(strings.Replace(authHeader, "Basic ", "", 1))
+		if err != nil {
+			status = 401
+			msg = "Auth header invalid format"
+		} else {
+			var credentials = strings.Split(string(decodedHeader), ":")
+			if len(credentials) != 2 {
+				status = 401
+				msg = "Auth header invalid format"
+			} else {
+				valid, err := validateCredentials(credentials[0], credentials[1])
+				if err != nil {
+					if err == sql.ErrNoRows {
+						status = 403
+						msg = "Username or password incorrect"
+					} else {
+						status = 400
+						msg = err.Error()
+					}
+				} else {
+					if valid {
+						status = 204
+					} else {
+						status = 403
+						msg = "Username or password incorrect"
+					}
+				}
+			}
+		}
+	} else {
+		status = 401
+		msg = "Missing 'Authorization' header"
+	}
+
+	return status, msg
+}
+
+func validateCredentials(username string, password string) (valid bool, err error) {
 	statement, err := db.Prepare(psCheckAuth)
 	if err != nil {
 		return false, err
--- routes/validateauth.go
+++ routes/validateauth.go
@@ -2,9 +2,6 @@ package routes
 
 import (
 	"context"
-	"database/sql"
-	"encoding/base64"
-	"strings"
 
 	"../database"
 	"../types"
@@ -13,41 +10,7 @@ import (
 
 // ValidateAuth validates credentials
 func ValidateAuth(ctx context.Context, request events.APIGatewayProxyRequest) (r types.Response, err error) {
-
-	if authHeader, ok := request.Headers["Authorization"]; ok {
-		decodedHeader, err := base64.StdEncoding.DecodeString(strings.Replace(authHeader, "Basic ", "", 1))
-		if err != nil {
-			r.StatusCode = 401
-			r.Message = "Auth header invalid format"
-		} else {
-			var credentials = strings.Split(string(decodedHeader), ":")
-			if len(credentials) != 2 {
-				r.StatusCode = 401
-				r.Message = "Auth header invalid format"
-			} else {
-				valid, err := database.CheckPassword(credentials[0], credentials[1])
-				if err != nil {
-					if err == sql.ErrNoRows {
-						r.StatusCode = 403
-						r.Message = "Username or password incorrect"
-					} else {
-						r.StatusCode = 400
-						r.Message = err.Error()
-					}
-				} else {
-					if valid {
-						r.StatusCode = 204
-					} else {
-						r.StatusCode = 403
-						r.Message = "Username or password incorrect"
-					}
-				}
-			}
-		}
-	} else {
-		r.StatusCode = 401
-		r.Message = "Missing 'Authorization' header"
-	}
+	r.StatusCode, r.Message = database.ProcessAuth(request.Headers)
 
 	return r, nil
 }
