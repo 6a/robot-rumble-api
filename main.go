5febcd919c6aed29d708c4877441d4ddbee45e9e
James Stanton <jestanton@outlook.com>
2019-12-10 01:46:57 +0000

    moved test func out of main func

--- main.go
+++ main.go
@@ -2,13 +2,45 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
+	"fmt"
+	"math/rand"
+	"time"
 
 	"./database"
 	"./routes"
 	"./types"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/rs/xid"
 )
+
+func createRandom(count int) {
+	ev := events.APIGatewayProxyRequest{}
+	ev.Headers = make(map[string]string)
+
+	for index := 0; index < count; index++ {
+		rand.Seed(time.Now().UTC().UnixNano())
+
+		name := xid.New().String()
+		pass := xid.New().String()
+
+		namelen := rand.Intn(12-2) + 2
+		name = name[len(name)-namelen:]
+
+		ev.Body = fmt.Sprintf(`{"name": "%v", "password": "%v"}`, name, pass)
+		_, _ = routes.CreateAccount(nil, ev)
+
+		ev.Headers["Authorization"] = fmt.Sprintf("Basic %v", base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%v:%v", name, pass))))
+
+		r1 := rand.Intn(50-0) + 0
+		r2 := rand.Intn(50-0) + 0
+		r3 := rand.Intn(50-0) + 0
+		ev.Body = fmt.Sprintf(`{"name": "%v", "wins": %v, "draws": %v, "losses": %v}`, name, r1, r2, r3)
+		r, _ := routes.Update(nil, ev)
+		fmt.Println(r)
+	}
+}
 
 func router(ctx context.Context, request events.APIGatewayProxyRequest) (r types.Response, err error) {
 	switch request.HTTPMethod {
@@ -31,20 +63,17 @@ func router(ctx context.Context, request events.APIGatewayProxyRequest) (r types
 func main() {
 	database.Init()
 
-	// ev := events.APIGatewayProxyRequest{}
-	// ev.Headers = make(map[string]string)
-
 	// ev.Headers["Authorization"] = "Basic NmE6YW5pbWFsMQ=="
 
 	// ev.Body = `{"name": "6a", "wins": 55, "draws": 0, "losses": 0}`
-
-	// res, err := routes.Update(nil, ev)
 
 	// if err != nil {
 	// 	fmt.Println(err)
 	// }
 
 	// fmt.Println(res)
+
+	// createRandom(4)
 
 	lambda.Start(router)
 }
